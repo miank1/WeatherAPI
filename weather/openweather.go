a667004edaaec2a0d567a33580483cba23a4c391
Ankit Mishra <ankit1703@gmail.com>
2025-06-08 22:38:21 +0530

    Drafted the interface and model code.

--- weather/openweather.go
+++ weather/openweather.go
@@ -1,1 +1,56 @@
 package weather
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+type OpenWeatherClient struct {
+	APIKey string
+}
+
+func NewOpenWeatherClient(apiKey string) *OpenWeatherClient {
+	return &OpenWeatherClient{
+		APIKey: apiKey,
+	}
+}
+
+func (ow *OpenWeatherClient) GetCurrentCityWeather(city string) (WeatherData, error) {
+	var data WeatherData
+
+	url := fmt.Sprintf("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/%s?unitGroup=us&key=%s&contentType=json",
+		city, ow.APIKey)
+
+	resp, err := http.Get(url)
+	if err != nil {
+		return data, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return data, fmt.Errorf("API request failed with status: %s", resp.Status)
+	}
+
+	var apiResp struct {
+		Name string `json:"name"`
+		Main struct {
+			Temp     float64 `json:"temp"`
+			Humidity int     `json:"humidity"`
+		} `json:"main"`
+		Weather []struct {
+			Description string `json:"description"`
+		} `json:"weather"`
+	}
+
+	err = json.NewDecoder(resp.Body).Decode(&apiResp)
+	if err != nil {
+		return data, err
+	}
+
+	data = WeatherData{
+		Description: apiResp.Name,
+	}
+
+	return data, nil
+}
--- weather/service.go
+++ weather/service.go
@@ -1,1 +1,5 @@
 package weather
+
+type WeatherService interface {
+	GetCurrentWeather(city string) (WeatherData, error)
+}
